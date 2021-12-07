c54e8a57507cc55b35f889030df7be3f891f4876
Kei Yoshida <kei_yoshida@brown.edu>
2021-12-07 11:07:29 -0500

    Update eval.py

--- eval.py
+++ eval.py
@@ -5,19 +5,17 @@ import tensorflow as tf
 
 def eval(model_path, test_path):
     model = tf.keras.models.load_model(model_path)
-
-    print("Evaluate on test data")
     results = model.evaluate(x_test, y_test, batch_size=128)
-    print("test loss:", results[0], " test accuracy:", results[1])
-
-    # print("Generate predictions for 5 samples")
-    # predictions = model.predict(x_test[:5])
-    # print("predictions shape:", predictions.shape)
+    loss = results[0]
+    accuracy = results[1]
+    
+    return (loss, accuracy)
 
 
 def main():
-    eval('CS1470_final_project/model_weights')
-
+    print("Evaluate on test data")
+    loss, accuracy = eval('CS1470_final_project/model_weights', ???)
+    print("test loss:", loss, " test accuracy:", accuracy)
 
 
 if __name__ == '__main__':
