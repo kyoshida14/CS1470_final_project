a6d9e80efff41f3dad598e54f9e778bdad354dfd
jktheophilus <71870338+jktheophilus@users.noreply.github.com>
2021-12-08 13:12:55 -0500

    Add files via upload
    completed eval to successfully test the gan detector model

--- eval.py
+++ eval.py
@@ -2,8 +2,9 @@ import os
 import csv
 import numpy as np
 import tensorflow as tf
+from readData import readData
 
-def eval(model_path, test_path):
+def eval(model_path, x_test, y_test):
     model = tf.keras.models.load_model(model_path)
     results = model.evaluate(x_test, y_test, batch_size=128)
     loss = results[0]
@@ -13,8 +14,14 @@ def eval(model_path, test_path):
 
 
 def main():
+    realDir = "biggan/0_real"
+    fakeDir = "biggan/1_fake"
+    (test_R, test_F) = readData(realDir, fakeDir, 0)
+    testTot = tf.concat([test_R, test_F], axis = 0)
+    testLabels = tf.concat([tf.zeros(tf.shape(test_R)[0]), tf.ones(tf.shape(test_F)[0])], axis = 0)
+
     print("Evaluate on test data")
-    loss, accuracy = eval('CS1470_final_project/model_weights', ???)
+    loss, accuracy = eval('CS1470_final_project/model_part2', testTot, testLabels)
     print("test loss:", loss, " test accuracy:", accuracy)
 
 
