7f6376ed77324a0b61be8372f8780d4df84401ea
Kei Yoshida <kei_yoshida@brown.edu>
2021-12-09 21:07:06 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,12 +9,12 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
 # used for the model created by Wang et al.
-from networks.resnet import resnet50
-import torch
-import torch.nn
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-from torch.autograd import Variable
+# from networks.resnet import resnet50
+# import torch
+# import torch.nn
+# import torchvision.transforms as transforms
+# import torchvision.datasets as datasets
+# from torch.autograd import Variable
 # from PIL import Image
 #from pytorch2keras import pytorch_to_keras
 from readData import readData1
@@ -24,7 +24,7 @@ input:
     img_path: e.g., 'examples/real.png'
     trained: use the weights that are only pretrained with imagenet (0=default), our model (1), or trained in the original paper (2)
 '''
-def demo(img_path, trained=1, model_path='weights/blur_jpg_prob0.5.pth'):
+def demo(img_path, trained=1, model_path='CS1470_final_project/model_partCD'):
     if (trained==0):    # load in ResNet50 with pre-trained weights with imagenet
         base_model = ResNet50(weights='imagenet', include_top=False, classes=1, pooling='max')
         # add a global spatial average pooling layer
@@ -49,44 +49,57 @@ def demo(img_path, trained=1, model_path='weights/blur_jpg_prob0.5.pth'):
         # get demo images
         demo_data = readData1(img_path)
         # get model
-        model = tf.keras.models.load_model(model_path)
-        predictions = model.predict(demo_data)
+        # model = tf.keras.models.load_model(model_path)
+        model = tf.saved_model.load(model_path)
+        fnc = model.signatures['serving_default']
+        # predictions = model.predict(demo_data)
 
-        return predictions
+        return fnc(demo_data)
 
-    # currently with error
-    elif (trained==2):   # convert pytorch model to keras
-        base_model = resnet50(num_classes=1)
-        state_dict = torch.load(model_path, map_location='cpu')
-        base_model.load_state_dict(state_dict['model'])
-        base_model
-        base_model.eval()
+    # with error
+    # elif (trained==2):   # convert pytorch model to keras
+    #     model_path='weights/blur_jpg_prob0.5.pth'
+    #     base_model = resnet50(num_classes=1)
+    #     state_dict = torch.load(model_path, map_location='cpu')
+    #     base_model.load_state_dict(state_dict['model'])
+    #     base_model
+    #     base_model.eval()
+    #
+    #     # create a dummy variable with correct shape, use it to trace the model
+    #     input_np = np.random.uniform(0,256,(1,3,256,256))
+    #     input_var = Variable(torch.FloatTensor(input_np))
+    #     model = pytorch_to_keras(base_model,input_var,[(3,None,None)],verbose=True)    # converted to keras
+    #
+    #     # transform images
+    #     img = image.load_img(img_path)  #target_size=(256,256)
+    #     x = image.img_to_array(img)     #(256, 256, 3)
+    #     x = np.expand_dims(x, axis=0)   #(1, 256, 256, 3)
+    #     x = preprocess_input(x)         #(1, 256, 256, 3)
+    #     x = tf.transpose(x, [0, 3, 1, 2])   # reshape to (1, 3, 256, 256)
+        # accuracy = model(x)
 
-        # create a dummy variable with correct shape, use it to trace the model
-        input_np = np.random.uniform(0,256,(1,3,256,256))
-        input_var = Variable(torch.FloatTensor(input_np))
-        model = pytorch_to_keras(base_model,input_var,[(3,None,None)],verbose=True)    # converted to keras
-
-        # transform images
-        img = image.load_img(img_path)  #target_size=(256,256)
-        x = image.img_to_array(img)     #(256, 256, 3)
-        x = np.expand_dims(x, axis=0)   #(1, 256, 256, 3)
-        x = preprocess_input(x)         #(1, 256, 256, 3)
-        x = tf.transpose(x, [0, 3, 1, 2])   # reshape to (1, 3, 256, 256)
-        accuracy = model(x)
-
-        return accuracy
+        # return accuracy
 
 def main():
     trained=1
-    predictions = demo('examples/realfakedir/1_fake', trained=1, model_path='model_part2')
+    real_img_path='CS1470_final_project/examples/realfakedir/0_real'
+    real_prediction_tensor = demo(real_img_path, trained)
+    # print(type(real_prediction_tensor))
+    fake_img_path='CS1470_final_project/examples/realfakedir/1_fake'
+    fake_prediction_tensor = demo(fake_img_path, trained)
+    # print(fake_prediction_tensor)
+    
     if (trained==0):
         prob = max(predictions)
-        print('probability that the Resnet-50 model can distinguish real or fake: {:.2f}%'.format(prob * 100))
+        print('probability that the model can distinguish real or fake: {:.2f}%'.format(prob * 100))
     elif (trained==1):
-        prob = predictions[0]
-        print(prob)
-        print('probability of being synthetic: {:.2f}%'.format(prob * 100))
+        for _, values in real_prediction_tensor.items():
+            prob1 = values.numpy()[0][1]   #[prob_real,prob_fake]
+        for _, values in fake_prediction_tensor.items():
+            prob2 = values.numpy()[0][1]   #[prob_real,prob_fake]
+        print('Example of real image: the model produced {:.2f}% probability of being synthetic '.format(prob1 * 100))
+        print('Example of fake image: the model produced {:.2f}% probability of being synthetic '.format(prob2 * 100))
+    # elif (trained==2):
 
     pass
 
